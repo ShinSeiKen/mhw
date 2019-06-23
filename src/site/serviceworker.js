0b512e0fdc4a7439af3e9f7739112561a0da704e
ShinSeiKen <shin+github@seiken.io>
2019-06-23 03:48:54 +0200

    Update serviceworker

--- src/site/serviceworker.js
+++ src/site/serviceworker.js
@@ -24,32 +24,33 @@ self.addEventListener('fetch', event => {
                 .then(response => { return reponse; })
                 .catch(error => { return caches.match(event.request); })
         );
-    }
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            if (response) {
-                return response;
-            }
-
-            return fetch(event.request).then(
-                response => {
-                    if(! response || response.status !== 200 || response.type !== 'basic') {
-                        return response;
-                    }
-
-                    // IMPORTANT: Clone the response. A response is a stream
-                    // and because we want the browser to consume the response
-                    // as well as the cache consuming the response, we need
-                    // to clone it so we have two streams.
-                    let responseToCache = response.clone();
-
-                    caches.open(PAGES_CACHE).then(cache => cache.put(event.request, responseToCache));
-
+    } else {
+        event.respondWith(
+            caches.match(event.request).then(response => {
+                if (response) {
                     return response;
                 }
-            );
-        })
-    );
+
+                return fetch(event.request).then(
+                    response => {
+                        if(! response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        let responseToCache = response.clone();
+
+                        caches.open(PAGES_CACHE).then(cache => cache.put(event.request, responseToCache));
+
+                        return response;
+                    }
+                );
+            })
+        );
+    }
 });
 
 // (3) Clear caches that we do not use anymore.
